f61b81bbe27f3267bfa1b7e407b8c045ad0d2ee0
Joshua Eichorn <joshua.eichorn@pagely.com>
2017-02-24 18:29:31 +0000

    sitemap_index.xml support

--- sitemap.lua
+++ sitemap.lua
@@ -5,14 +5,32 @@ local cjson = require "cjson"
 local requests = {}
 local offset = 1
 
+local function processUrlset(nodes)
+    for i, node in pairs(nodes) do
+        print("Found Url: ", node[1])
+        requests[#requests+1] = wrk.format('GET', node[1], {}, '')
+    end
+end
+
 init = function(args)
     local sitemap = args[0]
 
     local response = http.get(sitemap)
     local root = lom.parse(response.text)
-    local selected = xpath.selectNodes(root, '//url/loc')
-    for i, node in pairs(selected) do
-        requests[#requests+1] = wrk.format('GET', node[1], {}, '')
+
+    if root.tag == 'sitemapindex' then
+        print("Given a sitemap index, pulling in urlsets")
+        local selected = xpath.selectNodes(root, '//sitemap/loc')
+        for i, node in pairs(selected) do
+            print(node[1])
+            local response = http.get(node[1])
+            local doc = lom.parse(response.text)
+            local selected = xpath.selectNodes(doc, '//url/loc')
+            processUrlset(selected)
+        end
+    elseif root.tag == 'urlset' then
+        local selected = xpath.selectNodes(root, '//url/loc')
+        processUrlset(selected)
     end
 end
 
